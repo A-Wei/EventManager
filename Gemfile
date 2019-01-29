d88f440f04956e03ad06d0d595a63448d5d1327d
Wei Wei <weiwei.alex@gmail.com>
2019-01-29 16:42:52 +0100

    add shoulda matcher and factory bot and simple form

--- Gemfile
+++ Gemfile
@@ -15,18 +15,26 @@ gem 'simple_form'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
 gem 'webpacker'
+gem 'bcrypt'
 
 group :development, :test do
   gem 'rspec-rails'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'spring', '~> 2.0'
+  gem 'spring-commands-rspec', '~> 1.0'
+  gem 'spring-commands-rubocop', '~> 0.2'
+  gem 'cypress-on-rails', '~> 1.0'
+  gem 'dotenv-rails', '~> 2.1'
+  gem 'factory_bot_rails', '~> 4.8'
+  gem 'pry', '~> 0.10'
+  gem 'pry-byebug', '~> 3.4'
+  gem 'pry-rails', '~> 0.3'
 end
 
 group :development do
   gem 'guard', '~> 2.14', '>= 2.14.1'
   gem 'guard-livereload', '~> 2.5', '>= 2.5.2'
   gem 'listen', '>= 3.0.5', '< 3.2'
-  gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'web-console', '>= 3.3.0'
 end
 
@@ -34,6 +42,7 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'chromedriver-helper'
   gem 'selenium-webdriver'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -34,7 +36,13 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  Shoulda::Matchers.configure do |configure|
+    configure.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+# If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
