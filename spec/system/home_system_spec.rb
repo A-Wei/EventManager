4b0c74495a87478937e4bd8bef414c2435484ecc
Wei Wei <weiwei.alex@gmail.com>
2019-02-02 23:22:43 +0100

    rebase from update gem

--- spec/system/home_system_spec.rb
+++ spec/system/home_system_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe "home index page", type: :system do
   it 'renders hello world' do
     visit '/'
 
-    expect(page).to have_text('hello world')
+    expect(page).to have_text('Welcome to EventManager')
   end
 end
