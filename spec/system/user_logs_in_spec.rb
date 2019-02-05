e88e9ad9d3e4f8bce359ec8ac9066c2a56d3875b
Wei Wei <weiwei.alex@gmail.com>
2019-02-05 19:47:39 +0100

    Relocate build user subject to each test

--- spec/system/user_logs_in_spec.rb
+++ spec/system/user_logs_in_spec.rb
@@ -1,36 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe 'User logs in', type: :system do
-  subject { build(:user, email: 'test@example.com') }
-
   context 'when the login data is valid' do
     it 'allows the user to login and redirect to welcome page' do
+      build(
+        :user,
+        name: 'Bob',
+        email: 'bob@example.com',
+        password: 'foobar',
+        password_confirmation: 'foobar'
+      )
       visit '/login'
 
-      fill_in 'Email', with: 'test@example.com'
-      fill_in 'Password', with: 'password'
+      fill_in 'Email', with: 'bob@example.com'
+      fill_in 'Password', with: 'foobar'
       click_button 'Log in'
 
-      expect(page).to have_text('Hello Alice, Welcome to EventManager')
+      expect(page).to have_text('Hello Bob, Welcome to EventManager')
     end
   end
 
   context 'when the login data is incorrect' do
     it "shows 'Incorrect email or password, try again.' when email is incorrect" do
+      build(
+        :user,
+        name: 'Bob',
+        email: 'bob@example.com',
+        password: 'foobar',
+        password_confirmation: 'foobar'
+      )
       visit '/login'
 
       fill_in 'Email', with: 'test_user@example.com'
-      fill_in 'Password', with: 'password'
+      fill_in 'Password', with: 'foobar'
       click_button 'Log in'
 
       expect(page).to have_text('Incorrect email or password, try again.')
     end
 
     it "shows 'Incorrect email or password, try again.' when password is incorrect" do
+      build(
+        :user,
+        name: 'Bob',
+        email: 'bob@example.com',
+        password: 'foobar',
+        password_confirmation: 'foobar'
+      )
       visit '/login'
 
-      fill_in 'Email', with: 'test@example.com'
-      fill_in 'Password', with: 'foobar'
+      fill_in 'Email', with: 'bob@example.com'
+      fill_in 'Password', with: 'foobarr'
       click_button 'Log in'
 
       expect(page).to have_text('Incorrect email or password, try again.')
