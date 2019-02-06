9f419cd348e783dd2f3f687aa42cb051c1ce2cb5
Wei Wei <weiwei.alex@gmail.com>
2019-02-06 10:49:52 +0100

    Remove `log_in` method in SessionsController
    - The login form works without setting the session[:user_id], to keep
      the PR as small as possible, I decided to remove it and add it back
      when we really need it.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
-      log_in(user)
       redirect_to user_url(user)
     else
       flash[:error] = "Incorrect email or password, try again."
       render 'new'
     end
-  end
-
-  private
-
-  def log_in(user)
-    session[:user_id] = user.id
   end
 end
