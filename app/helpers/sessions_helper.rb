3ee191ef6e0172f1f22576f94ba180d368b8d846
Wei Wei <weiwei.alex@gmail.com>
2019-02-08 20:45:40 +0100

    Rewrite `current_user` method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,5 @@
 module SessionsHelper
   def current_user
-    if session[:user_id]
-      @_current_user = User.find(session[:user_id])
-    else
-      @_current_user = Guest.new
-    end
+    @_current_user ||= User.find_by_id(session[:user_id]) || Guest.new
   end
 end
