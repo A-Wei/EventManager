60fe61dba44a27933e09d445b0f5aae043249780
Wei Wei <weiwei.alex@gmail.com>
2019-02-08 16:59:09 +0100

    Update `current_user` to create guest object

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,9 @@
 module SessionsHelper
   def current_user
     if session[:user_id]
-      @_current_user ||= User.find(session[:user_id])
+      @_current_user = User.find(session[:user_id])
+    else
+      @_current_user = Guest.new
     end
   end
 end
