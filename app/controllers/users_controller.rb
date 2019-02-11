befbcca1136e38232b34532c551794b4e485144f
Wei Wei <weiwei.alex@gmail.com>
2019-02-11 14:18:26 +0100

    Add constraints for `edit` and `update`
    - User can only edit and update his/her own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  include SessionsHelper
+
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -11,6 +16,29 @@ class UsersController < ApplicationController
       render 'show'
     else
       render 'edit'
+    end
+  end
+
+  private
+
+  attr_reader :user
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    if !current_user.logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+
+    if @user != current_user
+      redirect_to root_url
     end
   end
 end
