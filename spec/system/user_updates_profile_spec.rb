2644924fee519dbac23d0ff71e201d0d2664fc89
Wei Wei <weiwei.alex@gmail.com>
2019-02-11 14:43:35 +0100

    Update spec context wording

--- spec/system/user_updates_profile_spec.rb
+++ spec/system/user_updates_profile_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User updates profile', type: :system do
   context 'when user is logged in' do
-    context 'and visit his/her own profile page' do
+    context 'when clicks profile button' do
       it 'shows a form and save changes made after click the update button' do
         user = create(:user, name: 'Alice')
 
@@ -15,7 +15,7 @@ RSpec.describe 'User updates profile', type: :system do
       end
     end
 
-    context "and visit other user's profile page" do
+    context "when visit other user's profile page" do
       it 'redirects to root_path' do
         user1 = create(:user, id: 1)
         create(:user, id: 2)
@@ -29,7 +29,7 @@ RSpec.describe 'User updates profile', type: :system do
   end
 
   context 'when user is not logged in' do
-    it 'redirects to log in when try to visit profile page' do
+    it 'redirects to login_path when try to visit profile page' do
       create(:user, id: 1)
 
       visit '/users/1/edit'
