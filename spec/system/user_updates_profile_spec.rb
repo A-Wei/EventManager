aa596034829c59bfe6798f0cf4a8aa42241e022e
Wei Wei <weiwei.alex@gmail.com>
2019-02-11 10:46:43 +0100

    Create user_updates_profile_spec.rb

---
+++ spec/system/user_updates_profile_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'User updates profile', type: :system do
+  context 'when user is logged in' do
+    context 'and visit his/her own profile page' do
+      it 'shows a form and save changes made after click the update button' do
+        user = create(:user, name: 'Alice')
+        login(user)
+        click_link 'Profile'
+
+        fill_in 'Name', with: 'Bob'
+
+        click_button 'Update'
+
+        expect(user.name).to eq('Bob')
+      end
+    end
+
+    context "and visit other user's profile page" do
+      it "redirects to user's own show page" do
+        user1 = create(:user, id: 1)
+        create(:user, id: 2)
+
+        login(user1)
+        visit'/users/2/edit'
+
+        expect(page.current_url).to eq('/users/1')
+      end
+    end
+  end
+
+  context 'when user is not logged in' do
+    it 'redirects to log in when try to visit profile page' do
+      user = create(:user, id: 1)
+
+      visit'/users/1/edit'
+
+      expect(page.current_path).to eq(login_path)
+    end
+  end
+end
