0fc23c3d53ce66cf28c1403816b4e028705f4901
Wei Wei <weiwei.alex@gmail.com>
2019-02-14 17:08:56 +0100

    Add `create` method in "password_reset_controller.rb"
    Not implementing the mailler at the moment.
    Will add that at later commit.

--- app/controllers/password_reset_controller.rb
+++ app/controllers/password_reset_controller.rb
@@ -3,5 +3,17 @@ class PasswordResetController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:password_reset][:email])
+
+    if @user
+      @user.create_password_reset_digest
+      render 'show'
+    else
+      flash[:error] = "User doesn't exist"
+      render 'new'
+    end
+  end
+
+  def show
   end
 end
