9474ec3e9044971af2ae74570f45ab8031b30369
Wei Wei <weiwei.alex@gmail.com>
2019-02-14 16:58:05 +0100

    Split `user_forgets_password.rb` into 2 spec files
    Split the tests into `user_forgets_password_spec.rb` and
    `user_resets_password_spec.rb` to make the spec contexts shorter
    and each spec will focus on part of the user behavior.

--- spec/system/user_forgets_password_spec.rb
+++ spec/system/user_forgets_password_spec.rb
@@ -1,74 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'User resets their password', type: :system do
+RSpec.describe 'User forgets their password', type: :system do
   context 'when user clicks on "forget password"' do
-    context 'when the submitted email exist and user clicks on "reset password link" in email' do
-      context 'when token and email are match in the link' do
-        it 'resets user password if the new password is valid and redirects to login page' do
-          user = create(:user)
-          new_password = 'new_password'
+    context 'when the submitted email exist' do
+      it 'sends an email to user email and notifies user' do
+        user = create(:user)
 
-          forget_password(user.email)
-          open_password_reset_page(user)
-          reset_password(user)
+        forget_password(user.email)
 
-          authenticated_user = user.authenticate(new_password)
-          expect(authenticated_user).to eq(user)
-          expect(page).to have_current_path(login_path)
-        end
-
-        it "show a 'password can't be blank' error if the new password is blank" do
-          user = create(:user)
-          new_password = ''
-
-          forget_password(user.email)
-          open_password_reset_page(user)
-          reset_password(new_password)
-
-          expect(page).to to_have_text("Password can't be blank")
-        end
-
-        it "show a 'password too short' error if the new password is too short" do
-          user = create(:user)
-          new_password = '12345'
-
-          forget_password(user.email)
-          open_password_reset_page(user)
-          reset_password(new_password)
-
-          expect(page).to to_have_text('Password is too short')
-        end
-
-        it "show a 'password doesn't math' error if the passwords does't match" do
-          user = create(:user)
-          new_password = 'new_password'
-
-          forget_password(user.email)
-          open_password_reset_page(user)
-          reset_password(password: new_password, password_confirmation: 'incorrect_password')
-
-          expect(page).to to_have_text("Password confirmation doesn't match Password")
-        end
-      end
-
-      context 'when token and email does not match in the link' do
-        it 'redirects to login page if token is incorrect' do
-          user = create(:user)
-
-          forget_password(user.email)
-          open_password_reset_page(user, token: 'wrong_token')
-
-          expect(page).to have_current_path(login_path)
-        end
-
-        it 'redirects to login page if email is incorrect' do
-          user = create(:user)
-
-          forget_password(user.email)
-          open_password_reset_page(user, email: 'wrong_email@example.com')
-
-          expect(page).to have_current_path(login_path)
-        end
+        expect(page).to have_text('Check your email')
       end
     end
 
@@ -76,7 +16,7 @@ RSpec.describe 'User resets their password', type: :system do
       it "shows a 'user doesn't exist' error" do
         forget_password('incorrect@example.com')
 
-        expect(page).to to_have_text("User doesn't exist")
+        expect(page).to have_text("User doesn't exist")
       end
     end
   end
---
+++ spec/system/user_resets_password_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe 'User resets their password', type: :system do
+  context 'when token and email address are correct in the url' do
+    it 'resets user password and redirects to login page if the new password is valid' do
+      user = create(:user)
+      new_password = 'new_password'
+
+      forget_password(user.email)
+      open_password_reset_page(user)
+      reset_password(user)
+
+      authenticated_user = user.authenticate(new_password)
+      expect(authenticated_user).to eq(user)
+      expect(page).to have_current_path(login_path)
+    end
+
+    context 'when the new password is incorrect' do
+      it "show a 'password can't be blank' error" do
+        user = create(:user)
+        new_password = ''
+
+        forget_password(user.email)
+        open_password_reset_page(user)
+        reset_password(new_password)
+
+        expect(page).to to_have_text("Password can't be blank")
+      end
+
+      it "show a 'password too short' error" do
+        user = create(:user)
+        new_password = '12345'
+
+        forget_password(user.email)
+        open_password_reset_page(user)
+        reset_password(new_password)
+
+        expect(page).to to_have_text('Password is too short')
+      end
+
+      it "show a 'password doesn't math' error" do
+        user = create(:user)
+        new_password = 'new_password'
+
+        forget_password(user.email)
+        open_password_reset_page(user)
+        reset_password(password: new_password, password_confirmation: 'incorrect_password')
+
+        expect(page).to to_have_text("Password confirmation doesn't match Password")
+      end
+    end
+  end
+
+  context 'when token or email address is incorrect in the url' do
+    it 'redirects to login page if token is incorrect' do
+      user = create(:user)
+
+      forget_password(user.email)
+      open_password_reset_page(user, token: 'wrong_token')
+
+      expect(page).to have_current_path(login_path)
+    end
+
+    it 'redirects to login page if email is incorrect' do
+      user = create(:user)
+
+      forget_password(user.email)
+      open_password_reset_page(user, email: 'wrong_email@example.com')
+
+      expect(page).to have_current_path(login_path)
+    end
+  end
+end
