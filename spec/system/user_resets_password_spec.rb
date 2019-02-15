0c1a9191d52d40c59cc3f3b0756b871c11a62cc0
Wei Wei <weiwei.alex@gmail.com>
2019-02-15 21:38:47 +0100

    Update spec
    Refactor arguments to instance variable and move into setup phase.
    Mock `password_reset_token` to return predifined token, so
    the token can be used in edit_password_reset_path.

--- spec/system/user_resets_password_spec.rb
+++ spec/system/user_resets_password_spec.rb
@@ -22,34 +22,40 @@ RSpec.describe 'User resets their password', type: :system do
       it "show a 'password can't be blank' error" do
         user = create(:user)
         new_password = ''
+        token = 'user_reset_token'
+        allow(User).to receive(:new_token).and_return(token)
 
         forget_password(user.email)
-        open_password_reset_page(user)
+        visit edit_password_reset_path(token, email: user.email)
         reset_password(new_password)
 
-        expect(page).to to_have_text("Password can't be blank")
+        expect(page).to have_text("Password can't be blank")
       end
 
       it "show a 'password too short' error" do
         user = create(:user)
         new_password = '12345'
+        token = 'user_reset_token'
+        allow(User).to receive(:new_token).and_return(token)
 
         forget_password(user.email)
-        open_password_reset_page(user)
+        visit edit_password_reset_path(token, email: user.email)
         reset_password(new_password)
 
-        expect(page).to to_have_text('Password is too short')
+        expect(page).to have_text('Password is too short')
       end
 
       it "show a 'password doesn't math' error" do
         user = create(:user)
-        new_password = 'new_password'
+        new_password = '123456'
+        token = 'user_reset_token'
+        allow(User).to receive(:new_token).and_return(token)
 
         forget_password(user.email)
-        open_password_reset_page(user)
+        visit edit_password_reset_path(token, email: user.email)
         reset_password(password: new_password, password_confirmation: 'incorrect_password')
 
-        expect(page).to to_have_text("Password confirmation doesn't match Password")
+        expect(page).to have_text("Password confirmation doesn't match Password")
       end
     end
   end
@@ -57,20 +63,26 @@ RSpec.describe 'User resets their password', type: :system do
   context 'when token or email address is incorrect in the url' do
     it 'redirects to login page if token is incorrect' do
       user = create(:user)
+      token = 'user_reset_token'
+      wrong_token = 'wrong_token'
+      allow(User).to receive(:new_token).and_return(token)
 
       forget_password(user.email)
-      open_password_reset_page(user, token: 'wrong_token')
+      visit edit_password_reset_path(wrong_token, email: user.email)
 
-      expect(page).to have_current_path(login_path)
+      expect(page).to have_current_path(root_path)
     end
 
     it 'redirects to login page if email is incorrect' do
       user = create(:user)
+      token = 'user_reset_token'
+      wrong_email = 'incorrect@example.com'
+      allow(User).to receive(:new_token).and_return(token)
 
       forget_password(user.email)
-      open_password_reset_page(user, email: 'wrong_email@example.com')
+      visit edit_password_reset_path(token, email: wrong_email)
 
-      expect(page).to have_current_path(login_path)
+      expect(page).to have_current_path(root_path)
     end
   end
 end
