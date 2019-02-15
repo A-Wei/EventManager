7fd706d074d6ce23a103c5c67d879aa68a3f2359
Wei Wei <weiwei.alex@gmail.com>
2019-02-15 12:52:36 +0100

    Update mailer config
    Update host to localhost
    Update default from: email to 'noreply@example.com'

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module EventManager
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    host = 'localhost:3000'
+    config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   end
 end
