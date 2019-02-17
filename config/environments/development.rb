f6c8fa6faa1b41f052616e83f35dc8e6556d354b
Wei Wei <weiwei.alex@gmail.com>
2019-02-17 21:05:25 +0100

    Set mailer default_url_options for test enviroment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,5 +66,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host}
+  config.action_mailer.default_url_options = { host: host }
 end
