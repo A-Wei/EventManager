112b93977efb57166043bf59a41ac87d6fb82d87
Wei Wei <weiwei.alex@gmail.com>
2019-02-18 11:29:03 +0100

    Update `user_mailer_spec.rb`
    Remove unnecessory context
    Fix unit test, was testing user.send_password_reset_email
    But should test UserMailer.password_reset

--- spec/mailer/user_mailer_spec.rb
+++ spec/mailer/user_mailer_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
   describe '#password_reset' do
-    context 'when user submits an email' do
-      it 'sends the password_reset mailer to the submitted email' do
-        user = create(:user)
-        user.password_reset_token = User.new_token
+    it 'sends the password_reset mailer to the submitted email' do
+      user = create(:user)
+      user.password_reset_token = User.new_token
 
-        mail = user.send_password_reset_email
+      mail = UserMailer.password_reset(user).deliver_now
 
-        expect(mail.to).to eq([user.email])
-        expect(mail.body.encoded).to match(user.password_reset_token)
-      end
+      expect(mail.to).to eq([user.email])
+      expect(mail.body.encoded).to match(user.password_reset_token)
     end
   end
 end
