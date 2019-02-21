8f3063a4e44d80aaa66c7b38945d351dbf15b34a
Wei Wei <weiwei.alex@gmail.com>
2019-02-21 14:08:32 +0100

    Add missing tests for `event` model

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  describe 'validations' do
+    describe '#title' do
+      it { is_expected.to validate_presence_of(:title) }
+    end
+
+    describe '#start_time' do
+      it { is_expected.to validate_presence_of(:start_time) }
+      it { is_expected.not_to allow_value('some_date').for(:start_time) }
+    end
+
+    describe '#end_time' do
+      it { is_expected.to validate_presence_of(:end_time) }
+      it { is_expected.not_to allow_value('some_date').for(:end_time) }
+    end
+
+    describe '#location' do
+      it { is_expected.to validate_presence_of(:location) }
+    end
+
+    describe '#description' do
+      it { is_expected.to validate_presence_of(:description) }
+    end
+  end
+end
