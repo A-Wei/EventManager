3d2748ecb868a1aa5e832af90342e9365c6ddd0f
Wei Wei <weiwei.alex@gmail.com>
2019-02-22 10:34:24 +0100

    Add missing test for time format
    Add a failling test if both start_time and end_time format are
    invalid.

--- spec/services/validate_event_spec.rb
+++ spec/services/validate_event_spec.rb
@@ -75,5 +75,15 @@ RSpec.describe ValidateEvent do
 
       expect(result.invalid_time_format?).to eq(true)
     end
+
+    it 'returns true if start_time and end_time format are both not correct' do
+      start_time = '2019/02/02 13:30'
+      end_time = '2019/02/02 14:30'
+      params = ActionController::Parameters.new(start_time: start_time, end_time: end_time)
+
+      result = ValidateEvent.call(params)
+
+      expect(result.invalid_time_format?).to eq(true)
+    end
   end
 end
