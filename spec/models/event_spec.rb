66d5cde1992fee98e290549632e15b3da1ef5f09
Wei Wei <weiwei.alex@gmail.com>
2019-02-22 10:31:53 +0100

    Add missing validation tests for Event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe Event, type: :model do
 
     describe '#start_time' do
       it { is_expected.to validate_presence_of(:start_time) }
+      it { is_expected.to allow_value('2019-02-02 12:00:00').for(:start_time) }
       it { is_expected.not_to allow_value('some_date').for(:start_time) }
     end
 
     describe '#end_time' do
       it { is_expected.to validate_presence_of(:end_time) }
+      it { is_expected.to allow_value('2019-02-02 12:00:00').for(:end_time) }
       it { is_expected.not_to allow_value('some_date').for(:end_time) }
     end
 
