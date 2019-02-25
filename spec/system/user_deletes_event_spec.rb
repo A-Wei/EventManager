7a3f42d1e88c068b8ea11c9a377f2291b2dcd1b9
Wei Wei <weiwei.alex@gmail.com>
2019-02-25 16:02:10 +0100

    Update 'user_deletes_event_spec' expectations

--- spec/system/user_deletes_event_spec.rb
+++ spec/system/user_deletes_event_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe 'Use deletes an event', type: :system do
     context 'when the user clicks "Delete" button' do
       it 'deletes the event and redirects to event index page' do
         user = create(:user)
-        event = create(:event, user: user)
+        event = create(:event, title: 'A test event', user: user)
 
         login(user)
         visit edit_event_path(event)
         click_button('Delete')
 
-        expect(Event.count).to eq(0)
+        expect(page).not_to have_text('A test event')
         expect(page).to have_current_path(events_path)
       end
     end
