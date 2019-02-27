b08569ae3f505addc36bfebf47ff37694787d25e
Wei Wei <weiwei.alex@gmail.com>
2019-02-27 11:21:15 +0100

    Refactor test

--- spec/system/user_searches_event_spec.rb
+++ spec/system/user_searches_event_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'User searches event', type: :system do
-  context 'when user searches events by a given term' do
-    it 'returns all events containing the given term in title' do
-      travel_to Time.new(2019, 2, 2, 10) do
-        create(:event, title: 'Test Event')
-      end
-      create(:event, title: 'Anohter Test')
-      create(:event, title: 'Some Event')
-      term = 'Test'
-
-      visit events_path
-      fill_in 'Search', with: term
-
-      expect(page).to have_text('Test')
-      expect(page).to have_text('Another Test')
-      expect(page).not_to have_text('Some Event')
+RSpec.describe 'User searches events', type: :system do
+  it 'returns all events containing the given term in title' do
+    travel_to Time.new(2019, 2, 2, 10) do
+      create(:event, title: 'Test Event')
     end
+    create(:event, title: 'Another Test')
+    create(:event, title: 'Some Event')
+
+    visit events_path
+    fill_in 'Term', with: 'Test'
+    click_button 'Submit'
+
+    expect(page).to have_text('Test Event')
+    expect(page).to have_text('Another Test')
+    expect(page).not_to have_text('Some Event')
   end
 end
